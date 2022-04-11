2c0d67fb9196d450bef015915b23bdf55217fb50
LearnPythonWithRune <learnpythonwithrune@gmail.com>
2022-04-11 16:12:05 +0200

    added files

---
+++ face_detection.py
@@ -0,0 +1,26 @@
+# importing opencv
+import cv2
+
+# using cv2.CascadeClassifier
+detector = cv2.CascadeClassifier("haarcascade_frontalface_default.xml")
+
+img = cv2.imread("sample_images/sample-00.jpg")
+
+# changing the image to gray scale for better face detection
+gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+multi_scale = detector.detectMultiScale(
+    gray,
+    scaleFactor=2,  # Big reduction
+    minNeighbors=5  # 4-6 range
+)
+
+# drawing a rectangle to the image.
+# for loop is used to access all the coordinates of the rectangle.
+for x, y, w, h in multi_scale:
+    cv2.rectangle(img, (x, y), (x+w, y+h), (0, 255, 0), 5)
+
+# showing the detected face followed by the waitKey method.
+cv2.imshow("image", img)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
---
+++ process_face_detection.py
@@ -0,0 +1,40 @@
+import os
+import cv2
+
+
+class FaceDetector:
+    def __init__(self):
+        self.detector = cv2.CascadeClassifier("haarcascade_frontalface_default.xml")
+        self.img = None
+
+    def read_image(self, filename):
+        self.img = cv2.imread(filename)
+
+    def detect_faces(self):
+        gray = cv2.cvtColor(self.img, cv2.COLOR_BGR2GRAY)
+
+        multi_scale = self.detector.detectMultiScale(
+            gray,
+            scaleFactor=2,  # Big reduction
+            minNeighbors=5  # 4-6 range
+        )
+
+        # drawing a rectangle to the image.
+        # for loop is used to access all the coordinates of the rectangle.
+        for x, y, w, h in multi_scale:
+            cv2.rectangle(self.img, (x, y), (x + w, y + h), (0, 255, 0), 5)
+
+        return self.img
+
+
+face_detector = FaceDetector()
+
+for filename in os.listdir('sample_images/'):
+    print(filename)
+    face_detector.read_image(f'sample_images/{filename}')
+    img = face_detector.detect_faces()
+
+    cv2.imshow("image", img)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
+
